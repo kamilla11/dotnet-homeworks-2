bf26c3757dc610211db1e908754e6bf8fdd9a05d
kamilla11 <gafarova.kamilla@list.ru>
2022-10-05 15:47:43 +0300

    Rewrited the calculator on F#

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -3,18 +3,39 @@
 open System
 open Hw4.Calculator
 
+type CalcOptions =
+    { arg1: float
+      arg2: float
+      operation: CalculatorOperation }
 
-type CalcOptions = {
-    arg1: float
-    arg2: float
-    operation: CalculatorOperation
-}
+let isArgLengthSupported (args: string []) = args.Length = 3
 
-let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+let parseOperation (arg: string) =
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> raise (ArgumentException("Incorrect operation!"))
 
-let parseOperation (arg : string) =
-    NotImplementedException() |> raise
-    
-let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+let parseCalcArguments (args: string []) =
+    if not (isArgLengthSupported (args)) then
+        raise (ArgumentException("Incorrect data. Length more than 3."))
+
+    let couldParse, val1 =
+        System.Double.TryParse(args[0])
+
+    if not (couldParse) then
+        raise (ArgumentException("Incorrect first argument!"))
+
+    let couldParse, val2 =
+        System.Double.TryParse(args[2])
+
+    if not (couldParse) then
+        raise (ArgumentException("Incorrect second argument!"))
+
+    let val3 = parseOperation args[1]
+
+    { arg1 = val1
+      arg2 = val2
+      operation = val3 }
--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,14 @@
-﻿raise (System.NotImplementedException())
+﻿open Hw4
+
+[<EntryPoint>]
+let main args =
+    let calc = Parser.parseCalcArguments args
+    let value1 = calc.arg1
+    let operation = calc.operation
+    let value2 = calc.arg2
+
+    let result =
+        Calculator.calculate value1 operation value2
+
+    printfn $"{result}"
+    0
